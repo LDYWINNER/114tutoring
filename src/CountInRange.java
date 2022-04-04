6ce40e702e3b3218c4e2de76fa652b33d3510c54
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-04-04 20:21:40 +0900

    4_04

---
+++ src/CountInRange.java
@@ -0,0 +1,25 @@
+public class CountInRange {
+
+    public static int countInRange (int[] integers, int a, int b) {
+        int upper, lower;
+        int total = 0;
+        if(a > b) {
+            upper = a;
+            lower = b;
+        } else {
+            upper = b;
+            lower = a;
+        }
+        for (int integer : integers) {
+            if(upper >= integer && lower <= integer) {
+                total ++;
+            }
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        int[] al = {14, 1, 22, 17, 36, 7, -43, 5};
+        System.out.println(countInRange(al, 17, 4));
+    }
+}
