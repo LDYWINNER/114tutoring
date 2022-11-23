b8a25dcdfe5b22358be53f0e06b77ff982fe5fd3
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-11-23 17:13:45 +0900

    11/23 Tutoring - 1

--- src/com/company/Scores.java
+++ src/com/company/Scores.java
@@ -1,28 +1,31 @@
 package com.company;
-
 import java.io.*;
-public class Scores {
+public class Scores
+{
     public static String readALine(BufferedReader input){
         String line = "";
-        try {
+        try{
             line = input.readLine();
         }
-        catch (IOException e) {
+        catch(IOException e){
             System.out.println("Oops");
         }
         return line;
     }
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException
+    {
         String input = "./scores.txt";
         BufferedReader br = new BufferedReader(new FileReader(input));
         PrintWriter output = new PrintWriter(new BufferedWriter(new FileWriter("scores.out")));
-        String line;
-        double totalsum;
+        String line = "";
+        double totalsum = 0;
 
-        while((line = readALine(br))!=null) {
+        while((line = readALine(br))!=null)
+        {
             String[] score = line.split(" ");
             totalsum = 0;
-            for(int i = 1; i < 4;i++) {
+            for(int i = 1; i < 4;i++)
+            {
                 totalsum += Integer.parseInt(score[i]);
             }
 
@@ -34,11 +37,10 @@ public class Scores {
             System.out.print(line);
             System.out.printf("     %.2f\n", avg);
             output.print(line);
-            output.printf(".2f\n", avg);
+            output.printf("%.2f\n", avg);
         }
 
         br.close();
         output.close();
     }
 }
-
