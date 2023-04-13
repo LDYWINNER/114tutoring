4eeee5495e467afc81e4e2c4817aae2af0d09086
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-04-13 14:45:54 +0900

    4/13 Tutoring - 1

--- src/com/company/EliminateDuplicates.java
+++ src/com/company/EliminateDuplicates.java
@@ -1,40 +1,43 @@
 package com.company;
 
 
-public class EliminateDuplicates {
+import java.util.Scanner;
+public class EliminateDuplicates{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of integers you will give me: ");
+        int size = sc.nextInt();
+        int[] inArray = new int[size];
 
-    public static String[] eliminateDuplicates(String str) {
-        String left = "";
-        String duplicates = "";
-
-        int temp = 0;
-        while(true) {
-            if(str.length() == 0) {
-                break;
-            } else {
-                for(int i = 0; i < str.length(); i++){
-
-                    if(str.charAt(i) == str.charAt(temp)) {
-
-                    }
-
-                }
-                temp ++;
-            }
+        System.out.print("Enter all of the integers now: ");
+        for (int i = 0; i < size; i++) {
+            inArray[i] = sc.nextInt();
         }
+        int[] outArray = eliminateDuplicates(inArray, size);
 
-
-        return new String[] {left, duplicates};
+        System.out.print("After elimination: ");
+        for (int i = 0; i < outArray.length; i++) {
+            System.out.print(outArray[i] + " ");
+        }
+        sc.close();
     }
 
-    public static void main(String[] args) {
-        String[] retStrings1 = eliminateDuplicates("abracadabra");
-        System.out.println((retStrings1[0] + ", " + retStrings1[1]));
-
-        String[] retStrings2 = eliminateDuplicates("Stony Brook University");
-        System.out.println((retStrings2[0] + ", " + retStrings2[1]));
-
-        String[] retStrings3 = eliminateDuplicates("This is a test sentence.");
-        System.out.println((retStrings3[0] + ", " + retStrings3[1]));
+    public static int[] eliminateDuplicates(int[] inArray, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (inArray[i] == inArray[j]) {
+                    for (int k = j; k < size - 1; k++) {
+                        inArray[k] = inArray[k + 1];
+                    }
+                    size--;
+                    j--;
+                }
+            }
+        }
+        int[] outArray = new int[size];
+        for (int i = 0; i < size; i++) {
+            outArray[i] = inArray[i];
+        }
+        return outArray;
     }
 }
